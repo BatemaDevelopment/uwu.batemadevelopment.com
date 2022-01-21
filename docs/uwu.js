cd4489bb2ae63f88acf3eea5a79fa8aaef615cd2
BatemaDevelopment <lukasbatema@gmail.com>
2022-01-21 14:39:18 -0500

    test

--- docs/uwu.js
+++
@@ -1,9 +0,0 @@
-function uwu() {
-  let randint = Math.round(Math.random()) * 3;
-
-  if (randint === 1) {
-    window.location.replace('https://batemadevelopment.com');
-  } else if (randint === 2) {
-    window.location.replace('https://dev.webalina.batemadevelopment.com');
-  }
-}
