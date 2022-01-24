c1c845d6a98dcf334f472eb1880cf3c33b156ffe
BatemaDevelopment <lukasbatema@gmail.com>
2022-01-24 11:50:09 -0500

    test

---
+++ docs/owo.js
@@ -0,0 +1,11 @@
+function uwu() {
+  let randint = Math.round(Math.random() * 3);
+
+  if (randint === 0) {
+    window.location.replace('https://www.batemadevelopment.com');
+  } else if (randint === 1) {
+    window.location.replace('https://dev.webalina.batemadevelopment.com');
+  } else if (randint === 2) {
+    window.location.replace('https://blinking.terminal.batemadevelopment.com');
+  }
+}
