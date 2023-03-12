bc17497e74c662e90a3a4613812fbadcdfe6b798
BatemaDevelopment <lukasbatema@gmail.com>
2023-03-11 21:48:00 -0500

    Update owo.js

--- docs/owo.js
+++ docs/owo.js
@@ -4,8 +4,8 @@ function uwu() {
   if (randint === 0) {
     window.location.replace('https://www.batemadevelopment.com');
   } else if (randint === 1) {
-    window.location.replace('https://dev.webalina.batemadevelopment.com');
+    window.location.replace('https://freeopenai.batemadevelopment.com');
   } else if (randint === 2) {
-    window.location.replace('https://blinking.terminal.batemadevelopment.com');
+    window.location.replace('https://luni.is-a.dev');
   }
 }
