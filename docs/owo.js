0507ef7e88c0d771fdfa6e8f8650a5ada7645834
BatemaDevelopment <lukasbatema@mg.batemadevelopment.com>
2023-03-11 22:02:07 -0500

    Update owo.js

--- docs/owo.js
+++ docs/owo.js
@@ -1,5 +1,5 @@
 function uwu() {
-  let randint = Math.round(Math.random() * 3);
+  let randint = Math.round(Math.random() * 4);
 
   if (randint === 0) {
     window.location.replace('https://www.batemadevelopment.com');
@@ -7,5 +7,7 @@ function uwu() {
     window.location.replace('https://freeopenai.batemadevelopment.com');
   } else if (randint === 2) {
     window.location.replace('https://luni.is-a.dev');
+  } else if (randint === 4) {
+    window.location.replace('https://blinkingterminal.batemadevelopment.com');
   }
 }
